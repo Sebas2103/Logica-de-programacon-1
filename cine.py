15bea8dc63577089eb3f617c9dedcae0781873c5
unknown <juanito2103@hotmail.es>
2024-09-04 10:51:45 -0500

    v5

---
+++ cine.py
@@ -0,0 +1,7 @@
+#Diseñar un programa en python  que calcule el valor a pagar por la entrada a cine de un grupos de personas
+valorPersonas=int(input("Ingrese la cantidad de personas que entraron al cine "))
+valorEntrada=int(input("Ingrese el valor de una entrada "))
+print("El valor de la cantidad de personas es",valorEntrada*valorPersonas)
+
+
+
