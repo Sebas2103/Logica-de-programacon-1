c57ae0ef6e0c2319dcf35b56863db3604b771b30
unknown <juanito2103@hotmail.es>
2024-09-04 09:50:41 -0500

    v3

--- ejercicios1.py
+++ ejercicios1.py
@@ -1,2 +1,18 @@
-nombre=input("¿Cuale es tu nombre")
-print("Me alegro de conocerte ",nombre)
+nombre=input("¿Cuale es tu nombre? ")
+
+print("Me alegro de conocerte",nombre)
+x=2.23
+#convertir una variable real  a entero
+
+X=2.23
+int(x)
+print(int(x))
+#Convertir una variable entero  a real
+x=300
+print(float(x))
+#Convertir real a complejo
+x=5.3
+print(complex(x))
+#Dos numeros complejos
+
+
