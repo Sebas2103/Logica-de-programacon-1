cb61eafcd28a64b2d73719afab6f969d6399b168
unknown <juanito2103@hotmail.es>
2024-09-04 10:39:48 -0500

    v4

--- ejercicios1.py
+++ ejercicios1.py
@@ -3,16 +3,60 @@ nombre=input("¿Cuale es tu nombre? ")
 print("Me alegro de conocerte",nombre)
 x=2.23
 #convertir una variable real  a entero
-
 X=2.23
 int(x)
 print(int(x))
 #Convertir una variable entero  a real
 x=300
 print(float(x))
-#Convertir real a complejo
+#Convertir real a complejos
 x=5.3
 print(complex(x))
-#Dos numeros complejos
+#Cadena de caracteres para saltar lineas
+print("""
+Hola soy 
+      sebas 
+      y el andres carne de res""")
+#Comillas  en un mensaje
+text="""comillas  triples
+para cadena de multiples lineas"""
+print(text)
+text="comillas dobles en un \"texto\""
+print(text)
+#comillas Dobles
+text="comillas dobles en un texto\"text\""
+
+print(text)
+#Comillas simples
+text="comillas dobles en un texto\'text\'"
+print(text)
+#Salto de linea
+text="salto \n linea\n linea"
+print(text)
+text="Tabulacion \ten \t python"
+print(text)
+#Para las listas
+text="Corchetes para indicar el indicar el indice o posicion"
+print("Posicion esta la letra: ",text[4])
+#Para indicar que \n hace parte del string
+text=r"En una raw string no se interpreta  \n nk hay salto de linea"
+print(text)
+#Sumar dos cadenas de texto 
+text="Se concatena poniendo "+"al lado la otra "
+#Pregunta  si la cadena de texto hace parte o la contiene 
+text="Con in se comprueba si hay sub-cadena"
+print("sub esta en cadena "in text)
+print("sub" in text)
+text="Con * se repite la cadena"
+
+text="La funcion len() devuelve la longitud de la cadena"
+print(len(text))
+#lo convierte a string
+text=str(10)
+
+
+
+
+
 
 
