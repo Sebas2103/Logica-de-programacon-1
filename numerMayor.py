9678296f0030d426bfd378f056e3455cd4c3ac48
unknown <juanito2103@hotmail.es>
2024-09-09 13:54:52 -0500

    v15

--- numerMayor.py
+++ numerMayor.py
@@ -1,4 +1,4 @@
-umero1= int(input("Ingrese el primer numero 1 "))
+numero1= int(input("Ingrese el primer numero 1 "))
 numero2= int(input("Ingrese el primer numero 2 "))
 numero3= int(input("Ingrese el primer numero 3 "))
 
@@ -8,3 +8,5 @@ if numero2>numero1 and numero2>numero3 :
     print("El numero mayor es ",numero2)
 if numero3>numero1 and numero3>numero2 :
     print("El numero mayor es ",numero3)
+else:
+    print("Los numeros son iguales")
