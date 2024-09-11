3facec79319f663edb11f3825491ed6c6d2ff2ef
unknown <juanito2103@hotmail.es>
2024-09-11 13:32:35 -0500

    v21

--- tablademultiplicaar.py
+++ tablademultiplicaar.py
@@ -1,6 +1,6 @@
 """ tabla de multiplica del 1 a 10 cual numero   """
 numero = float(input(""" Ingrese un numero """))
-i = 0
+i = 1
 print("""   Tabla de multiplicar    """)
 while i <= 10:
     print(f"| {i}*{numero}= {int(numero*i)} |")
