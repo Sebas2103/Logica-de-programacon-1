85ea5208b2660b102384badbca4d168e8e268609
unknown <juanito2103@hotmail.es>
2024-09-11 09:28:01 -0500

    31

---
+++ ciclowhile.py
@@ -0,0 +1,4 @@
+conteo = 0
+while conteo <= 10:
+    print(f"contador ={conteo} ")
+    conteo+=1
---
+++ tablademultiplicaar.py
@@ -0,0 +1,7 @@
+""" tabla de multiplica del 1 a 10 cual numero   """
+numero = float(input(""" Ingrese un numero """))
+i = 0
+print("""   Tabla de multiplicar    """)
+while i <= 10:
+    print(f"| {i}*{numero}= {int(numero*i)} |")
+    i += 1
