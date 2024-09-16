e0bfb185012e82163decc7adcfa3dc3fdfcaa0e8
unknown <juanito2103@hotmail.es>
2024-09-16 11:01:19 -0500

    v39

--- while2.py
+++ while2.py
@@ -1,1 +1,19 @@
-#Ingresar varias notas
+#Ingresar varias notas y calcular el promedio.s para ingresar nueva nota n para salir
+opcion="s"
+cont=1
+acumulador=0
+while opcion!="n":
+      
+    nota=int(input(f""" Ingrese una nota  #{cont}  """))
+    cont+=1
+    acumulador=nota+acumulador
+  
+    opcion=input("Ingrese la n para salir y s para continuar ingresando notas ")
+    
+print(f"su promedio es: {acumulador/(cont-1)}")
+
+
+
+
+
+
