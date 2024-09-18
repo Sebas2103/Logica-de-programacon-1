0f6e778748745fa364597ece5ea790b6246a26ab
unknown <juanito2103@hotmail.es>
2024-09-18 09:50:34 -0500

    v46

---
+++ while5.py
@@ -0,0 +1,12 @@
+#Diseñar un algoritmo que calcule el promedio de los pesos de un grupo de gimnastas.Se desconoce cuantos gimnastas conforman el grupo           
+cont=1
+opcion="n"
+acumulador=0
+while opcion!="s":
+    peso=int(input("Ingrese el peso del primer gimnastas"))
+    acumulador+=peso
+    opcion=input(""" Desea salir s o n """)
+    cont+=1
+
+print(f""" El promedio del peso de gimnastas es 
+      {acumulador/cont} """)
