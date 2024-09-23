498aa98e297e8a4ede084b987e5c434c499275f2
unknown <juanito2103@hotmail.es>
2024-09-23 09:17:22 -0500

    first commit

---
+++ ciclofor.py
@@ -0,0 +1,8 @@
+for i in range(5):
+    print(i)
+print(""" Un para metro indica elnumero de veces que se repite comenzando desde el 0 """)
+
+#El valor fi8nal de la mitad siempre es el numero -1 ,el numero final indica el incremento y el principio indica el valor que va tomar i
+for i in range (1,5,2):
+    print(i)
+print(""" tres  ´parametro indican el valor inicial,final y el incremento """)
