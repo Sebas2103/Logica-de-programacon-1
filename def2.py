62b1ddc4bd1882682b90650f72336e7a992a06b4
unknown <juanito2103@hotmail.es>
2024-09-25 10:46:30 -0500

    seven commit

---
+++ def2.py
@@ -0,0 +1,8 @@
+def resta (num1,num2):
+    res=num1-num2
+    return res
+resultado=resta(10,3)
+a,b=10,3
+print("El resultado de la resta {}- {}= {}".format(a,b,resultado))
+resultado=resta(num2=3,num1=10)
+print("El resultado de la resta {}- {}= {}".format(a,b,resultado))
