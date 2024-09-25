c09bf4f0c07a41f79220cb9068e0941ab872faaf
unknown <juanito2103@hotmail.es>
2024-09-25 10:02:04 -0500

    five commit

---
+++ personas.py
@@ -0,0 +1,5 @@
+#7. Clasificar a 5 personas según la edad y el genero, al final deberá informar 
+# la cantidad de personas masculinas mayores de edad, la cantidad de personas femeninas menores de edad, 
+# cantidad de personas mayores y menores. Además, 
+# deberá indicar el porcentaje que representa las 
+# personas mayores y menores de edad.
