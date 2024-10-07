bcb6968d451d52ad1c6b51001f9add0a2c4ea7e0
unknown <juanito2103@hotmail.es>
2024-10-07 10:37:04 -0500

    cuarto

---
+++ areglos.py
@@ -0,0 +1,8 @@
+lista=[1,2,3,6,20,-20]
+print(lista)
+print(lista[1:5])
+for i in lista: #lista[i]
+    print(i)
+listatotal=lista+lista
+print(f""" lista total {listatotal} """)
+print (f""" lista multiplicada por 3 {listatotal*3} """)
